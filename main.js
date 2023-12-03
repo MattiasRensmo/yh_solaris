b578dee5931ce6dafd669adaee71b82b54a4b6e9
Mattias Rensmo <mattias@rensmo.se>
2023-12-04 00:13:53 +0100

    Nu med textshadow för ökad läsbarhet

--- main.js
+++ main.js
@@ -71,6 +71,16 @@ domSolarSystem.addEventListener('click', createDetailsPage)
 domSolarSystem.addEventListener('mouseover', showPlanetName)
 domDetailsClose.addEventListener('click', closeDetails)
 
+const root = document.documentElement
+
+document.addEventListener('mousemove', (evt) => {
+  let x = evt.clientX / innerWidth
+  let y = evt.clientY / innerHeight
+
+  root.style.setProperty('--mouse-x', x)
+  root.style.setProperty('--mouse-y', y)
+})
+
 /*
  * Program Start
  */
